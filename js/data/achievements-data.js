e4bdfc4573a7b8e0484d55579ad2e10defc8c473
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-04-15 15:42:08 +0300

    Achievements HTML

---
+++ js/data/achievements-data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        icon:'archive',
+        number: 245,
+        title: 'Total projects'
+    },
+    {
+        icon:'trophy',
+        number: 535,
+        title: 'Trophies won'
+    },
+    {
+        icon: 'users',
+        number: 288,
+        title: 'Experienced staff'
+    },
+    {
+        icon: 'smile-o',
+        number: 750,
+        title: 'Happy clients'
+    }
+]
+
+export default data;
--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 "use strict";
 import SectionHeading from './SectionHeading.js';
+import Achievements from './Achievements.js';
 
 
+//Headings
 // find all attr with 'data-h2' to create H2 elements
 const allDataH2 = document.querySelectorAll('[data-h2]');
 for ( let i=0; i<allDataH2.length; i++ ) {
     new SectionHeading( allDataH2[i] );
 }
+
+//Achievements
+new Achievements('#achievements > .row');
