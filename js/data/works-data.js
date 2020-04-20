5976f20d1e526de36952710c9c9f84bf1fbadf3d
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-04-20 15:26:04 +0300

    works gallery

---
+++ js/data/works-data.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const data = [
+    {
+        photo: '1.jpg',
+        name: 'Studio & Art',
+        categories: ['Branding', 'Creative Work', 'Web Design']
+    },
+    {
+        photo: '2.jpg',
+        name: 'Creative & Art',
+        categories: ['Branding', 'Creative Work', 'Web Design', 'Wordpress']
+    },
+    {
+        photo: '3.jpg',
+        name: 'Open Imagination',
+        categories: ['Branding', 'Creative Work', 'Wordpress']
+    },
+    {
+        photo: '4.jpg',
+        name: 'Locked Steel Gate',
+        categories: ['Branding', 'Wordpress', 'Mobile App', 'Responsive Design']
+    },
+    {
+        photo: '5.jpg',
+        name: 'Mac Sunglasses',
+        categories: ['Mobile App', 'Responsive Design', 'Web Design', 'SEO', 'Wordpress']
+    },
+    {
+        photo: '6.jpg',
+        name: 'Backpack Contents',
+        categories: ['Responsive Design', 'SEO', 'Wordpress']
+    },
+    {
+        photo: '7.jpg',
+        name: 'Wilderness Culture',
+        categories: ['Creative Work', 'Web Design', 'SEO']
+    },
+    {
+        photo: '8.jpg',
+        name: 'Travel Diaries',
+        categories: ['Creative Work', 'SEO', 'Responsive Design']
+    },
+    {
+        photo: '9.jpg',
+        name: 'Wanderlust',
+        categories: ['Mobile App', 'SEO', 'Responsive Design']
+    }
+]
+
+export default data;
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 "use strict";
-import SectionHeading from './SectionHeading.js';
-import Achievements from './Achievements.js';
+import SectionHeading from './components/SectionHeading.js';
+import Achievements from './components/Achievements.js';
+import Gallery from './components/Gallery.js';
 
 
 //Headings
@@ -10,5 +11,6 @@ for ( let i=0; i<allDataH2.length; i++ ) {
     new SectionHeading( allDataH2[i] );
 }
 
-//Achievements
 new Achievements('#achievements > .row');
+
+new Gallery ('#works > .row:nth-child(3) > .col-12');
